840d0bf0f7b7780046f02dccbe1c3d8ebe61b710
Austin <497133212@qq.com>
2020-07-24 15:42:23 +0800

    [Parking Lot][Austin] feature: add should_fetch_car_when_fetch_from_parking_lot_given_car_ticket test

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -55,4 +55,29 @@ public class ParkingLotTest {
         assertNotNull(secondCarTicket);
         assertNotNull(thirdCarTicket);
     }
+
+    @Test
+    void should_fetch_multiple_car_when_fetch_multiple_car_from_parking_lot_given_multiple_car_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        Car thirdCar = new Car();
+        CarTicket firstCarTicket = parkingLot.park(firstCar);
+        CarTicket secondCarTicket = parkingLot.park(secondCar);
+        CarTicket thirdCarTicket = parkingLot.park(thirdCar);
+
+        //when
+        Car fetchFirstCar = parkingLot.fetch(firstCarTicket);
+        Car fetchSecondCar = parkingLot.fetch(secondCarTicket);
+        Car fetchThirdCar = parkingLot.fetch(thirdCarTicket);
+
+        //then
+        assertNotNull(fetchFirstCar);
+        assertNotNull(fetchSecondCar);
+        assertNotNull(fetchThirdCar);
+        assertEquals(firstCar, fetchFirstCar);
+        assertEquals(secondCar, fetchSecondCar);
+        assertEquals(thirdCar, fetchThirdCar);
+    }
 }
