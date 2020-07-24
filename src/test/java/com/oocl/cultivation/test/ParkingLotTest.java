0edff2177beff7c73ae64a686e736df64274365d
Austin <497133212@qq.com>
2020-07-24 19:01:20 +0800

    [Parking Lot][Austin] refactor: refactor the parking and ParkingTest and add ParkingBoy

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -5,93 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
-    @Test
-    void should_return_ticket_when_park_given_car() {
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car();
-
-        //when
-        CarTicket ticket = parkingLot.park(car);
-
-        //then
-        assertNotNull(ticket);
-    }
-
-    @Test
-    void should_fetch_car_when_fetch_from_parking_lot_given_car_ticket() {
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car();
-        CarTicket carTicket = parkingLot.park(car);
-
-        //when
-        Car fetchCar = parkingLot.fetch(carTicket);
-
-        //then
-        assertNotNull(car);
-        assertEquals(car, fetchCar);
-
-    }
-
-    @Test
-    void should_paking_multiple_car_when_park_given_car() {
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-        Car firstCar = new Car();
-        Car secondCar = new Car();
-        Car thirdCar = new Car();
-
-        //when
-        CarTicket firstCarTicket = parkingLot.park(firstCar);
-        CarTicket secondCarTicket = parkingLot.park(secondCar);
-        CarTicket thirdCarTicket = parkingLot.park(thirdCar);
-
-
-        //then
-        assertNotNull(firstCarTicket);
-        assertNotNull(secondCarTicket);
-        assertNotNull(thirdCarTicket);
-    }
-
-    @Test
-    void should_fetch_multiple_car_when_fetch_multiple_car_from_parking_lot_given_multiple_car_ticket() {
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-        Car firstCar = new Car();
-        Car secondCar = new Car();
-        Car thirdCar = new Car();
-        CarTicket firstCarTicket = parkingLot.park(firstCar);
-        CarTicket secondCarTicket = parkingLot.park(secondCar);
-        CarTicket thirdCarTicket = parkingLot.park(thirdCar);
-
-        //when
-        Car fetchFirstCar = parkingLot.fetch(firstCarTicket);
-        Car fetchSecondCar = parkingLot.fetch(secondCarTicket);
-        Car fetchThirdCar = parkingLot.fetch(thirdCarTicket);
-
-        //then
-        assertNotNull(fetchFirstCar);
-        assertNotNull(fetchSecondCar);
-        assertNotNull(fetchThirdCar);
-        assertEquals(firstCar, fetchFirstCar);
-        assertEquals(secondCar, fetchSecondCar);
-        assertEquals(thirdCar, fetchThirdCar);
-    }
 
     @Test
     void should_return_no_car_when_fetch_car_from_parking_lot_given_wrong_car_ticket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
-        Car car = new Car();
-        CarTicket carTicket = parkingLot.park(car);
         CarTicket wrongTicket = new CarTicket();
 
         //when
-        Car noCar = parkingLot.fetch(wrongTicket);
+        Car car = parkingLot.fetch(wrongTicket);
 
         //then
-        assertNull(noCar);
+        assertNull(car);
     }
 
     @Test
@@ -125,7 +50,7 @@ public class ParkingLotTest {
     @Test
     void should_return_no_ticket_when_parking_lot_reach_max_capacity_given_car() {
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         for(int i=1;i<=10;i++) {
             Car car = new Car();
             parkingLot.park(car);
@@ -138,5 +63,4 @@ public class ParkingLotTest {
         //then
         assertNull(carTicket);
     }
-
 }
