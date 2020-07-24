4df089d1c2596d0f15250ab55f9d6c9d031f3727
Austin <497133212@qq.com>
2020-07-24 15:27:07 +0800

    [Parking Lot][Austin] feature: implement ParkingLot park method

--- src/main/java/com/oocl/cultivation/test/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/test/ParkingLot.java
@@ -1,8 +1,14 @@
 package com.oocl.cultivation.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private Map<CarTicket, Car> parkingRooms = new HashMap<>();
     public CarTicket park(Car car) {
-        return new CarTicket();
+        CarTicket carTicket = new CarTicket();
+        parkingRooms.put(carTicket, car);
+        return carTicket;
     }
 
     public Car fetch(CarTicket carTicket) {
